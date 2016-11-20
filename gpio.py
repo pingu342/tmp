cea6b4463f703db492327ebd402f689eafdd9baf
pingu342 <pingu342@users.noreply.github.com>
2016-11-21 01:22:42 +0900

    gpio.py

---
+++ gpio.py
@@ -0,0 +1,28 @@
+#coding: UTF-8
+
+import RPi.GPIO as GPIO
+import time
+
+print "Hello RPi.GPIO"
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+led = 14
+GPIO.setup(led, GPIO.OUT)
+GPIO.output(led, 0)
+
+print "ready?"
+raw_input()
+
+i = 0
+while i<10000:
+    print "touchesBegan"
+    GPIO.output(led, 1)
+    time.sleep(0.5)
+    print "touchesEnded"
+    GPIO.output(led, 0)
+    time.sleep(0.5)
+    i += 1
+
+GPIO.cleanup()
